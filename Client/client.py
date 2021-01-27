c91b68aed03c2d9e609b6a69c6806d6f8e79cfef
Lucas Longarini <37842688+LucasLongarini@users.noreply.github.com>
2021-01-26 22:42:39 -0700

    simplified receive function

--- Client/client.py
+++ Client/client.py
@@ -3,57 +3,53 @@ import socket
 class MySocket:
     def __init__(self, sock=None):
         print('init')
+        self.FORMAT = 'utf-8'
         if sock is None:
             self.sock = socket.socket(
                             socket.AF_INET, socket.SOCK_STREAM)
         else:
             self.sock = sock
 
+    # Connects the socket to a host and port
     def connect(self, host, port):
         self.sock.connect((host, port))
 
+    # Closes the socket
     def close(self):
         self.sock.close()
 
+    # Sends a string message throught the socket encoded in utf-8
     def send(self, msg):
         totalsent = 0
-        # while totalsent < MSGLEN:
-            # sent = self.sock.send(msg[totalsent:])
-        sent = self.sock.send(msg.encode('utf-8'))
+        sent = self.sock.send(msg.encode(self.FORMAT))
         if sent == 0:
             raise RuntimeError("socket connection broken")
         totalsent = totalsent + sent
 
+    # Waits to recieve data in the socket and returns it as a string
     def receive(self):
-        chunks = []
-        bytes_recd = 0
-        # while bytes_recd < MSGLEN:
-        # chunk = self.sock.recv (min(MSGLEN - bytes_recd, 2048))
-        chunk = self.sock.recv(1024)
-        if chunk == b'':
-            chunks = "exit"
-            return chunks 
-            # raise RuntimeError("socket connection broken")
-        chunks.append(chunk)
-        bytes_recd = bytes_recd + len(chunk)
-        return b''.join(chunks)
+        msg = self.sock.recv(1024).decode(self.FORMAT)
+        if msg: 
+            return str(msg)
+        else:
+            return ''
         
 socket = MySocket()
 socket.connect('192.168.0.23', 55921)
 while True:
-            data = socket.receive()
-            print(data)
-            if not data:
-                continue
-            if data == b'get id\n':
-                print('get id')
-                socket.send('30031173\n')
-                # break
-            if data == b'get name\n':
-                print('get name')
-                socket.send('Shiv Odedra\n')
-                # break
-            if data == "exit":
-                print('closing socket')
-                socket.close()
-                break
+    data = socket.receive()
+    print(data)
+    if not data:
+        continue
+    if data == 'get id\n':
+        print('get id')
+        socket.send('30031173\n')
+        # break
+    if data == 'get name\n':
+        print('get name')
+        socket.send('Shiv Odedra\n')
+        # break
+    if data == 'close':
+        print('closing socket')
+        socket.close()
+        break
